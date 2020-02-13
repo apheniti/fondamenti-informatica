5b6ba5d895f28d85dee3e9910ba12f5dc500671c
Chiara <apheniti@gmail.com>
2020-02-13 22:31:59 +0100

    Delete Esercizio23.java

--- Esercizio23.java
+++
@@ -1,8 +0,0 @@
-/*Modificare il programma Java dell'esercizio 2.1 usando il minor numero possibile di variabili.*/
-
-public class Esercizio23{
-	public static void main(String[] args){
-		String nome = "apheniti";
-		System.out.println(nome.substring(0,1)+nome.substring(nome.length()-1));
-	}
-}
